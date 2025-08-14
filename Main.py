5a2998a1acdba3f8d0c30ee9adeeb7e07c59546f
mhuzaifa555 <huzaifazahid843@gmail.com>
2025-08-14 16:36:15 +0500

    first day of Python practice code

---
+++ Main.py
@@ -0,0 +1,21 @@
+# this is my first code
+# print("i like pizza")
+# print ("i like burger too")
+#these all are strings except age
+name = "huzaifa"
+email = "huzaifaisleraning@gmail.com"
+# INTEGERS
+phone = 32213213213
+age = 20
+#float
+GPA = 3.0
+CGPA = 2.9
+#Boolean
+nationalaity = False
+# if nationalaity:
+#     print("YOU ARE A PAKISTANI")
+# else:
+#     print("You ARE NOT PAKISTANI")
+# mobile = input("Which mobile are you using?: ")
+# price = int(input("its price is?: "))
+# print(f"WOHOOOO, your mobile is {mobile} and is amazingly at ${price}")
