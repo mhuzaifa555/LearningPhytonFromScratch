5cda5bc0f2e0cca045e4562953df6d4606482349
mhuzaifa555 <huzaifazahid843@gmail.com>
2025-08-15 12:57:18 +0500

    practing if staments

--- Main.py
+++ Main.py
@@ -49,12 +49,66 @@ nationalaity = False
 
 
 # if statments
+# age = int(input("Please Enter your age: "))
+# if age > 18:
+#     print("You can access this.")
+#
+# elif age < 0:
+#     print("You havent been born yet")
+# else:
+#     print("You must be above 18")
+# If Statement Practice Challenges
+# Even or Odd
+
+# value = int(input("Please enter a value to check, if its odd or even (value =/ 0): "))
+# if value ==0:
+#     print("The value can not be 0")
+# elif value % 2 == 0:
+#     print("The value is Even")
+# else:
+#     print("The value is Odd")
+#
+# Leap Year Checker
+
+# year = int(input("Enter a year to check if its a leap year or not: "))
+# # if year < 0:
+# #     print("Year can't be negative")
+# # elif year % 400 ==0:
+# #     print("Its a leap year")
+# # elif year % 100 ==0:
+# #     print("Its not a leap year")
+# # elif year % 4 ==0:
+# #     print("Its a leap year")
+# # else:
+# #     print("Its not a leap year")
+
+# Password Lenght Checker
+
+# password = input("Please enter a password: ")
+#
+# if len(password) < 6:
+#     print("Please enter a password consisting of minimum 6 characters")
+# elif len(password) > 15:
+#     print("Please enter a password consisting of maximum 15 characters")
+# else:
+#     print("Success, Your password has been set")
+
+# Mini Login System
+
+# username = "huzaifa"
+# password = "coding231"
+#
+# user = input("Enter your username: ")
+#
+# if user==username:
+#      pass1 = input("Enter your password: ")
+#      if pass1 == password:
+#         print("Successfully logged in!")
+#      else:
+#         print("Can not logged in, Incorrect Password!")
+# else:
+#     print("username does not exist")
 
 
- age = input("You can access this website")
-if age > 18:
-elif age =0:
-    print("You havent been born yet")
-else:
-    print("You must be above 18")
+
 
